7e5f02a15d047d15888dbf05e78a7d9eb0b87c4a
Jay <jay.sanjay.karia@gmail.com>
2021-09-04 20:39:51 +0530

    Done

--- Learning Code/Example 02/scrappy.py
+++ Learning Code/Example 02/scrappy.py
@@ -1,24 +1,38 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('https://coreyms.com/').text
 soup = BeautifulSoup(source, 'lxml')
 
-article  = soup.find('article')
-# print(article.prettify())
+csv_file = open('cms_scrape.csv', 'w')
 
-headline = article.h2.a.text
-# print(headline)
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video_link'])
 
-summary = article.find('div', class_='entry-content').p.text
-# print(summary)
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
 
-vid_source = article.find('iframe', class_='youtube-player')['src']
-# print(vid_source)
+    summary = article.find('div', class_='entry-content').p.text
+    print(summary)
 
-vid_id = vid_source.split('/')[4]
-vid_id = vid_id.split('?')[0]
-print(vid_id)
 
-# Time: 32:40
+    try:
+        vid_source = article.find('iframe', class_='youtube-player')['src']
+
+        vid_id = vid_source.split('/')[4]
+        vid_id = vid_id.split('?')[0]
+        print(vid_id)
+
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
+    except:
+        yt_link = None
+    print(yt_link)
+
+    print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
