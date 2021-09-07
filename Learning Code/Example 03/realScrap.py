480023c3397af3c5ded6aa1a99491109e11eb1d1
Jay <jay.sanjay.karia@gmail.com>
2021-09-07 11:37:00 +0530

    completed

--- Learning Code/Example 03/realScrap.py
+++ Learning Code/Example 03/realScrap.py
@@ -1,21 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
-html = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+print('Put some skills you are not familiar with')
+unfamiliar_skills = input('>')
+print(f'Filtering Out {unfamiliar_skills}')
 
-soup = BeautifulSoup(html, 'lxml')
-# print(soup.prettify())
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-for job in jobs:
-    when = soup.find('span', class_='sim-posted').text
-    if 'few' in when:
-        company_name = soup.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-        skills = soup.find('span', class_='srp-skills').text.replace(' ', '')
+def find_jobs():
+    html = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
-        print(f'''
-        Company Name: {company_name}
-        Skills Required: {skills}
-        Posted: {when}
-        ''')
+    soup = BeautifulSoup(html, 'lxml')
+    # print(soup.prettify())
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        when = job.find('span', class_='sim-posted').text
+        if 'few' in when:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            link = job.header.h2.a['href']
+            if unfamiliar_skills not in skills:
+                with open(f'{index}.txt', 'w') as f:
+                    f.write(f"Company Name: {company_name.strip()}\n")
+                    f.write(f"Required Skills: {skills.strip()}\n")
+                    f.write(f"More Info: {link}")
+                print(f'File save: {index}')
 
-        print('-'*20)
+if __name__ == "__main__":
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f"\nWaiting {time_wait} minutes...\n")
+        time.sleep(time_wait * 60)
+        print()
