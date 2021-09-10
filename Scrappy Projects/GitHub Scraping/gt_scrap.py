ce6c0b0df2b118a1acd12ca44d462fcd2ed237e8
Jay <jay.sanjay.karia@gmail.com>
2021-09-10 18:35:08 +0530

    50% issue solved!

--- Scrappy Projects/GitHub Scraping/gt_scrap.py
+++ Scrappy Projects/GitHub Scraping/gt_scrap.py
@@ -18,34 +18,41 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # try:
 for repos in soup.find_all('div', id="user-repositories-list"):
+
+    repo_name = repos.find('a', itemprop='name codeRepository')
+    name = repo_name.get_text().strip()
+    repo_url = repos.find('a', itemprop='name codeRepository')['href']
+    url = f'https://github.com//{repo_url}'
+    u = repos.find('relative-time', class_='no-wrap')
+    update = u.get_text().strip()
+    v = repos.find('span', class_='Label')
+    visibility = v.get_text().strip()
+    print()
+    print(name)
+    print(url)
+    print(update)
+    print(visibility)
     try:
-        if " " not in repos:
-            repo_name = repos.find('a', itemprop='name codeRepository')
-            print(repo_name.get_text().strip())
+        language = repos.find(itemprop='programmingLanguage')
+        lang = language.get_text().strip()
+        print(lang)
+    except AttributeError:
+        print("None")
 
-            repo_url = repos.find('a', itemprop='name codeRepository')['href']
-            url = f'https://github.com//{repo_url}'
-            print(url)
+    try:
+        s = repos.find('a', class_='Link--muted mr-3')
+        stars = s.get_text().strip()
+        print(stars)
+    except:
+        print("None")
 
-            description = repos.find('p', class_='col-9 d-inline-block color-text-secondary mb-2 pr-4').get_text().strip()
+    try:
+        d = repos.find('p', class_='col-9 d-inline-block color-text-secondary mb-2 pr-4')
+        description = d.get_text().strip()
+        print(description)
+    except:
+        print("None")
 
-            update = repos.find('relative-time', class_='no-wrap')
-            print(update.get_text().strip())
-
-            stars = repos.find('a', class_='Link--muted mr-3')
-            print(stars.get_text().strip())
-
-            visibility = repos.find('span', class_='Label')
-            print(visibility.get_text().strip())
-
-
-            lang = repos.find(itemprop='programmingLanguage')
-            print(lang.get_text().strip())
-            print(description)
-        else: print("None")
-    except AttributeError as ae:
-        # repos = " "
-        pass
     # writer.writerow([person_name.encode('utf-8'), repo_name.encode('utf-8'), url.encode('utf-8'), lang.encode('utf-8'), update.encode('utf-8'), stars.encode('utf-8'), visibility.encode('utf-8'), description.encode('utf-8')])
 # except:
 #     print("\nAn error ocurred while retriving the data. Sorry!")
