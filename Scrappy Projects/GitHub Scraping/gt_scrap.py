0e30996bd357240b3f6d87c738f5d8c1289c3879
Jay <jay.sanjay.karia@gmail.com>
2021-09-10 08:46:52 +0530

    Bug!!!

--- Scrappy Projects/GitHub Scraping/gt_scrap.py
+++ Scrappy Projects/GitHub Scraping/gt_scrap.py
@@ -11,19 +11,31 @@ print(link)
 html = requests.get(link).text
 soup = BeautifulSoup(html, 'html.parser')
 
+file = open(f'{person_name}-info.csv', 'w')
+writer = csv.writer(file)
+
+writer.writerow(["Username", "Repo Name", "Repo URL", "Language", "Last Updated", "Stars", "Visibility", "Description"])
+
 try:
-    repos = soup.find('div', id='user-repositories-list')
+    for repos in soup.find_all('div', id='user-repositories-list'):
 
-    repo_name = repos.find_all(itemprop='name codeRepository')
+        repo_name = repos.find(itemprop='name codeRepository')
 
-    lang = repos.find_all(itemprop='programmingLanguage')
+        repo_url = repos.find('a', itemprop='name codeRepository')['href']
+        url = f'https://github.com//{repo_url}'
 
-    update = repos.find_all('relative-time', class_='no-wrap')
+        lang = repos.find(itemprop='programmingLanguage')
 
-    stars = repos.find_all('a', class_='mr-3')
+        update = repos.find('relative-time', class_='no-wrap')
 
-    visibility = repos.find_all('span', class_='Label')
+        stars = repos.find('a', class_='mr-3')
 
-    description = repos.find_all('p', class_='col-9 d-inline-block color-text-secondary mb-2 pr-4') 
+        visibility = repos.find('span', class_='Label')
+
+        description = repos.find('p', class_='col-9 d-inline-block color-text-secondary mb-2 pr-4')
+
+        writer.writerow([person_name, name, url, lang, update, stars, visibility, description])
 except:
     print("\nAn error ocurred while retriving the data. Sorry!")
+
+file.close()
