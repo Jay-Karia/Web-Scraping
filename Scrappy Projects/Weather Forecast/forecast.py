5ec1c0fe6a297cf5a3b9b45c57b4fdeb0902d3e0
Jay <jay.sanjay.karia@gmail.com>
2021-09-17 14:31:40 +0530

    weekend forecast remaining

--- Scrappy Projects/Weather Forecast/forecast.py
+++ Scrappy Projects/Weather Forecast/forecast.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
 import csv
-
 
 def DailyForecast():
     try:
@@ -36,15 +36,53 @@ def DailyForecast():
         print("An Unexpected Error occurred, Sorry!")
 
 def HourlyForecast():
-    url = 'https://weather.com/en-IN/weather/hourbyhour/l/efdc1f9159c1023991dbc88aa4fd4592aa13eec53c0937a0e793693ac7bb82c3'
+    try:
+        url = 'https://weather.com/en-IN/weather/hourbyhour/l/efdc1f9159c1023991dbc88aa4fd4592aa13eec53c0937a0e793693ac7bb82c3'
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        file = open('Hourly-Forecast.csv', 'w')
+        csvWriter = csv.writer(file)
+
+        csvWriter.writerow(["Sr no.", "Time", "Temperature", "Weather", "Rain %", "Wind Speed"])
+
+        mainDiv = soup.find('div', class_='HourlyForecast--DisclosureList--3CdxR')
+        Lists = mainDiv.find_all('details', class_='DaypartDetails--DayPartDetail--1up3g Disclosure--themeList--25Q0H')
+
+        for index,items in enumerate(Lists):
+            index = index + 1
+            t = items.find('h2', class_='DetailsSummary--daypartName--2FBp2').get_text().strip()
+            s = datetime.strptime(t, "%H:%M")
+            time = s.strftime("%r")
+            temperature = items.find('span', class_='DetailsSummary--tempValue--1K4ka').get_text().strip()
+            weather = items.find('span', class_='DetailsSummary--extendedData--365A_').get_text().strip()
+            rainPer = items.find('div', class_='DetailsSummary--precip--1ecIJ').span.get_text().strip()
+            wind = items.find('span', class_='Wind--windWrapper--3aqXJ undefined').get_text().strip().split(' ')[1]+' km/h'
+
+            csvWriter.writerow([index, time, temperature, weather, rainPer, wind])
+
+        file.close()
+
+        print("File Saved Successfully!!")
+    except:
+        print("An unexpected Error occurred, Sorry!")
+
+def WeekendForecast():
+    url = 'https://weather.com/en-IN/weather/weekend/l/efdc1f9159c1023991dbc88aa4fd4592aa13eec53c0937a0e793693ac7bb82c3'
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
 
-    mainDiv = soup.find('div', class_='HourlyForecast--DisclosureList--3CdxR')
-    Lists = mainDiv.find_all('details', class_='DaypartDetails--DayPartDetail--1up3g Disclosure--themeList--25Q0H')
+    mainDiv = soup.find('section', class_='card Card--card--HiWPW WeekendForecast--Card--XkPCS')
+    weekend = mainDiv.find('div', class_='WeekendForecast--DisclosureList--2hLEm')
+    Lists = weekend.find_all('details', class_='Disclosure--themeList--25Q0H')
 
-    for index,items in enumerate(Lists):
-        time = t.find('h2', class_='DetailsSummary--daypartName--2FBp2').get_text().strip()
-        print(time)
+option = ["Daily Forecast", "Hourly Forecast", "Weekend Forecast"]
 
-HourlyForecast()
+# print(f"Select: \n1) {option[0]}\n2) {option[1]}")
+# sel = input()
+
+# if sel == "1":
+#     DailyForecast()
+# elif sel == "2":
+#     HourlyForecast()
+WeekendForecast()
