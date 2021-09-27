5ddb7cf92b0f6c5ed437b881bc6c4896f9f26f0e
Jay <jay.sanjay.karia@gmail.com>
2021-09-27 20:12:51 +0530

    Update

--- Learning Code/Dynamic Scraping/My Tests/quotes_scrap.py
+++ Learning Code/Dynamic Scraping/My Tests/quotes_scrap.py
@@ -1,14 +1,24 @@
 from selenium import webdriver
+import csv
 
 driver = webdriver.Chrome(executable_path='C:\\Programming\\Chrome WebDriver\\app\\chromedriver')
 url = 'https://quotes.toscrape.com/'
 driver.get(url)
 
+file = open('quotes.csv', 'w')
+writer = csv.writer(file)
+
+writer.writerow(["Sr. no.", "Quote", "Author"])
+
 try:
 	items = driver.find_elements_by_class_name('quote')
 
-	for item in items:
-		title = item.find_element_by_class_name('text').text
+	for index, item in enumerate(items):
+		index += 1
+		quote = item.find_element_by_class_name('text').text
 		author = item.find_element_by_class_name('author').text
+		writer.writerow([index, quote, author])
 finally:
 	driver.close()
+
+file.close()
