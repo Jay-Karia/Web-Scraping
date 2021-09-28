c593560262112018aa7355fd423f125409fba29b
Jay <jay.sanjay.karia@gmail.com>
2021-09-28 21:31:56 +0530

    all info got!!!

--- Scrappy Projects/JetBrains Scraping/jet_scrap.py
+++ Scrappy Projects/JetBrains Scraping/jet_scrap.py
@@ -9,8 +9,13 @@ mainDiv = driver.find_element_by_xpath('//*[@id="products-page"]/div/div[1]/div[
 items = mainDiv.find_elements_by_class_name('_wt-card_ru6f9_1')
 
 try:
-    for item in items:
+    for index, item in enumerate(items):
+        index = index + 1
         product_name = item.find_element_by_class_name('wt-link_hardness_hard').text
-        print(product_name)
+        description = item.find_element_by_class_name('wt-offset-top-12').text
+        version = item.find_element_by_class_name('wt-text-2_theme_light').text
+        if '.' not in version:
+            version = "None"
+        url = item.find_element_by_class_name('wt-link_hardness_hard').get_attribute('href')
 finally:
     driver.close()
