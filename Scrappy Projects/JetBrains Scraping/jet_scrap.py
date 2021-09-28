a6f1ed9591a310e340dc7db5cbfcfce7da6938fa
Jay <jay.sanjay.karia@gmail.com>
2021-09-28 14:04:26 +0530

    got the names!!!

--- Scrappy Projects/JetBrains Scraping/jet_scrap.py
+++ Scrappy Projects/JetBrains Scraping/jet_scrap.py
@@ -6,9 +6,11 @@ url = 'https://www.jetbrains.com/products/'
 driver.get(url)
 
 mainDiv = driver.find_element_by_xpath('//*[@id="products-page"]/div/div[1]/div[1]')
-items = mainDiv.find_elements_by_tag_name('div')
+items = mainDiv.find_elements_by_class_name('_wt-card_ru6f9_1')
 
 try:
-    print(mainDiv.text)
+    for item in items:
+        product_name = item.find_element_by_class_name('wt-link_hardness_hard').text
+        print(product_name)
 finally:
     driver.close()
