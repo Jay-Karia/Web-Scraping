01a068298ed3a7874118fdd6c025dc23f7ea0a52
Jay <jay.sanjay.karia@gmail.com>
2021-09-29 11:58:43 +0530

    update

--- Scrappy Projects/Upwork Scraping/upwork_scrap.py
+++ Scrappy Projects/Upwork Scraping/upwork_scrap.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path='C:\\Programming\\Chrome WebDriver\\app\\chromedriver')
 url = 'https://www.upwork.com/search/profiles/?q=python'
@@ -11,7 +12,11 @@ try:
     for item in items:
         # index = index + 1
         person_name = item.find_element_by_class_name('identity-name').text
-        print(person_name)
+        profession = item.find_element(by=By.CLASS_NAME, value='freelancer-title').text
+        location = item.find_element(by=By.CLASS_NAME, value='vertical-align-middle').text
+        r = item.find_element_by_class_name('profile-stats')
+        rate = item.find_element(by=By.TAG_NAME, value='strong').text+'/hr'
+        print(rate)
 
 finally:
     driver.close()
